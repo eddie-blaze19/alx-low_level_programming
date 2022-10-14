f1e7e7d4e23fb8a652f80cb6b9ecb6fad6969487
Edward Dziworshie <edwarddziworshie@gmail.com>
2022-10-13 21:09:06 -0700

    eh

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-/* this c program starts with the main function to print using the puts function */
+/** this c program starts with the main 
+*   function to print using 
+*   the puts function 
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
