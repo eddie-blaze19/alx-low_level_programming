16e0c647e30a7dde5c953a5564efd033d16938eb
Edward Dziworshie <edwarddziworshie@gmail.com>
2022-10-16 10:09:06 -0700

    work ad done

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,7 +14,7 @@ for (i = 0; i < 99; i++)
 for (j = i + 1; j <= 99; j++)
 {
 putchar('0' + i / 10);
-putchar('0' + i)
+putchar('0' + i);
 putchar(' ');
 putchar('0' + j / 10);
 putchar('0' + j % 10);
