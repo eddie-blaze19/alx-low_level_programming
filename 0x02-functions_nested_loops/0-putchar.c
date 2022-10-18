8eefeba14ee0e03a44104deb2c19e6f9bc5240a3
Edward Dziworshie <edwarddziworshie@gmail.com>
2022-10-18 03:58:21 -0700

    c task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main - This is the start of the program
+* Description: printf program
+* Return: the return value is 0
+*/
+int main(void)
+{
+printf("_putchar");
+return (0);
+}
