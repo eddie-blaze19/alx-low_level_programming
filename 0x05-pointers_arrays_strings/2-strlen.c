7698ba41b3258b0c8e49fbf34ca541579d8ccecc
Edward Dziworshie <edwarddziworshie@gmail.com>
2022-10-24 13:53:01 -0700

    task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,16 +1,15 @@
 #include "main.h"
 /**
- * _strlen - return the length of a string
- * 
- * @s: char to check
- * Description: this will return the length of a string
- * Return: val 0
- */
+* _strlen - return the length of a string
+* @s: char to check
+* Description: this will return the length of a string
+* Return: val of a
+*/
+
 int _strlen(char *s)
 {
-	int a = 0;
-
-	for (; *s++;)
-		a++;
-	return (a);
+int a = 0;
+for (; *s++;)
+a++;
+return (a);
 }
