3b3885965bf53e17ce0c48b88a658541f3e8afb0
Edward Dziworshie <edwarddziworshie@gmail.com>
2022-10-24 14:15:42 -0700

    task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,16 +2,13 @@
 /**
 * _puts - prints a string
 * @str: the string
-*
-* Return: the length of the string
 */
 void _puts(char *str)
 {
-int i = 0;
-while (str[i] != '\0')
+int i;
+for (i = 0; str[i] != '\0'; i++)
 {
-_putchar(str[i]);
-i++;
+	_putchar(str[i]);
 }
-_putchar('\n');
+        _putchar('\n');
 }
